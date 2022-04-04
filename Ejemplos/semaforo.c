a6de0508a892579df0f63036b92ae16c35abfdc9
Axel García <axldorian3@gmail.com>
2022-04-04 00:56:16 -0500

    Agregado programa chat

---
+++ Ejemplos/semaforo.c
@@ -0,0 +1,70 @@
+/****  Programa para mostrar el uso de semáforos en la sincronización de procesos, tomado de la página 389 del libro de: UNIX Programación Avanzada. F.M. Márquez ******/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define SEM_HIJO 0
+#define SEM_PADRE 1
+int main(int argc, char *argv[])
+{
+	int i = 10, semid;
+	pid_t pid;
+	struct sembuf operacion;
+	key_t llave;
+
+	llave = ftok(argv[0], 'a');
+	if ((semid = semget(llave, 2, IPC_CREAT | 0600)) == -1)
+	{
+		perror("semget");
+		exit(-1);
+	}
+	semctl(semid, SEM_HIJO, SETVAL, 0);	 // cerrar semáforo
+	semctl(semid, SEM_PADRE, SETVAL, 1); // abrir semáforo
+
+	if ((pid = fork()) == -1)
+	{
+		perror("fork");
+		exit(-1);
+	}
+	else if (pid == 0)
+	{
+		while (i)
+		{
+			// Cerramos el semáforo del proceso hijo
+			operacion.sem_num = SEM_HIJO;
+			operacion.sem_op = -1;
+			operacion.sem_flg = 0;
+			semop(semid, &operacion, 1);
+			printf("Proceso hijo:%d\n", i--);
+			// Abrimos el semáforo del proceso padre
+			operacion.sem_num = SEM_PADRE;
+			operacion.sem_op = 1;
+			semop(semid, &operacion, 1);
+		}
+		// Borramos el semáforo
+		semctl(semid, 0, IPC_RMID, 0);
+	}
+	else
+	{
+		while (i)
+		{
+			// Cerramos el semáforo del proceso padre
+			operacion.sem_num = SEM_PADRE;
+			operacion.sem_op = -1;
+			operacion.sem_flg = 0;
+			semop(semid, &operacion, 1);
+			printf("Proceso padre:%d\n", i--);
+			// Abrimos el semáforo del proceso hijo
+			operacion.sem_num = SEM_HIJO;
+			operacion.sem_op = 1;
+			semop(semid, &operacion, 1);
+		}
+		// Borramos el semáforo
+		semctl(semid, 0, IPC_RMID, 0);
+	}
+	return EXIT_SUCCESS;
+}
--- Primer Parcial/chat.c
+++ Primer Parcial/chat.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <semaphore.h>
-// #include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define MAX_LONG 255
+#define SEM_HIJO 0
+#define SEM_PADRE 1
+
 /*
 	fd[0]: lectura
 	fd[1]: escritura
@@ -15,69 +19,142 @@
 	fd2 = hijo 
 */
 
-int main(){
+int main(int argc, char *argv[]){
 
 	pid_t hijo;
-	int n, estado, fd1[2], fd2[2];
-	char buffer1[MAX_LONG], buffer2[MAX_LONG];
+	long n_padre, n_hijo;
+	int estado, fd1[2], fd2[2], semid, bandera = 0;
+	char buffer1_w[MAX_LONG], buffer1_r[MAX_LONG], buffer2_w[MAX_LONG], buffer2_r[MAX_LONG];
 	char *p;
 
-    sem_t *sem1 = sem_open("/semaforo1", 0x0008,  0644, 0);
-    sem_t *sem2 = sem_open("/semaforo2", 0x0008,  0644, 1);
+	struct sembuf operacion;
+	key_t llave;
 
+	// configuración de semaforos
+	llave = ftok(argv[0], 'a');
+	if ((semid = semget(llave, 2, IPC_CREAT | 0600)) == -1){
+		perror("semget");
+		exit(-1);
+	}
+
+	semctl(semid, SEM_PADRE, SETVAL, 0);	// cerrar semáforo
+	semctl(semid, SEM_HIJO, SETVAL, 1);		// abrir semáforo
+
+	// creación de pipes
 	if (pipe(fd1) < 0 || pipe(fd2) < 0){
-		perror("Error de Pipe");
-		exit(1);
-	} else {
-		hijo = fork();
+		perror("Pipe");
+		exit(-1);
+	}
 
-		// hijo
-		if (hijo == 0){
-			while (1){
-				sem_wait(sem1);
+	hijo = fork();
 
-				printf("Padre entrada: "); 
-				fgets(buffer2, MAX_LONG, stdin);
-				
-				p = strchr(buffer2, '\n');
-				if (p != NULL){
-					*p = '\0';
-				}
+	// hijo
+	if (hijo == 0){
+		while (1){
+    		// Cerramos el semáforo del proceso hijo
+    		operacion.sem_num = SEM_HIJO;
+    		operacion.sem_op = -1;
+    		operacion.sem_flg = 0;
+    		semop(semid, &operacion, 1);
 
-				write(fd2[1], buffer2, MAX_LONG);	// escribir en pipe
+			// Sección de lectura y escritura del hijo
+			// --------------------------------------------------
 
-				sem_post(sem2);
+			// bandera que indica que es la primer ejecución del ciclo
+			// para evitar problemas en la lectura del pipe
+			if (bandera == 0){
+				bandera = 1;
+			} else {
+				// lee mensaje de padre
+				n_padre = (long)read(fd1[0], buffer2_r, MAX_LONG);
+				printf("\nMi padre dijo: %s\n", buffer2_r);
 
-				if (strstr(buffer2, "mata kondo") != NULL){
+				// verifica si mensaje de padre es palabra clave
+				if (strstr(buffer2_r, "mata kondo") != NULL){
 					break;
 				}
 			}
-		}
-		
-		// padre
-		else {
-			while (1){
-				sem_wait(sem2);
 
-				n = read(fd2[0], buffer1, MAX_LONG);
-				
-				printf("Mi hijo dijo: %s\n", buffer1);
-				printf("Chars leídos: %d\n", n);
+			// lee de teclado mensaje de hijo
+			printf("Hijo entrada: "); 
+			fgets(buffer2_w, MAX_LONG, stdin);
 
-				sem_post(sem1);
+			// reemplaza fin de linea leido por el fin de cadena
+			p = strchr(buffer2_w, '\n');
+			if (p != NULL){
+				*p = '\0';
+			}
 
-				if (strstr(buffer1, "mata kondo") != NULL){
-					exit(0);
-				}
+			// escribir en pipe
+			write(fd2[1], buffer2_w, MAX_LONG);
+
+			// --------------------------------------------------
+
+    		// Abrimos el semáforo del proceso padre
+    		operacion.sem_num = SEM_PADRE;
+    		operacion.sem_op = 1;
+    		semop (semid, &operacion, 1);
+
+			// verifica si el mensaje del hijo es palabra clave
+			if (strstr(buffer2_w, "mata kondo") != NULL){
+				break;
 			}
 		}
-	}
 
-	// cerrar semaforos
-    sem_close(sem1);
-    sem_unlink("/semaforo1");
-    sem_close(sem2);
-    sem_unlink("/semaforo2");
+		// Borramos el semáforo
+		semctl(semid, 0, IPC_RMID,0);
+	}
+		
+	// padre
+	else {
+		while (1){
+			// Cerramos el semáforo del proceso padre
+			operacion.sem_num = SEM_PADRE;
+			operacion.sem_op = -1;
+			operacion.sem_flg = 0;
+			semop(semid, &operacion, 1);
+
+			// Sección de lectura y escritura del padre
+			// --------------------------------------------------
+
+			// lee mensaje de hijo
+			n_padre = (long)read(fd2[0], buffer1_r, MAX_LONG);				
+			printf("\nMi hijo dijo: %s\n", buffer1_r);
+
+			// verifica si mensaje de hijo es palabra clave
+			if (strstr(buffer1_r, "mata kondo") != NULL){
+				break;
+			}
+
+			// lee de teclado mensaje de padre
+			printf("Padre entrada: "); 
+			fgets(buffer1_w, MAX_LONG, stdin);
+
+			// reemplaza fin de linea leido por el fin de cadena	
+			p = strchr(buffer1_w, '\n');
+			if (p != NULL){
+				*p = '\0';
+			}
+
+			// escribir en pipe
+			write(fd1[1], buffer1_w, MAX_LONG);
+
+			// --------------------------------------------------
+
+			// Abrimos el semáforo del proceso hijo
+			operacion.sem_num = SEM_HIJO;
+			operacion.sem_op = 1;
+			semop (semid, &operacion, 1);
+
+			// verifica si el mensaje del padre es palabra clave
+			if (strstr(buffer1_w, "mata kondo") != NULL){
+				break;
+			}
+		}
+
+		// Borramos el semáforo
+ 		semctl(semid, 0, IPC_RMID,0);
+	}
 
     return EXIT_SUCCESS;
 }
