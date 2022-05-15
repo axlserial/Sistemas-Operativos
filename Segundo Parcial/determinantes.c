7111839251faaa6ceb07b7e8fa50a64563fe4c7d
Maggy30ev <maggy30ev@gmail.com>
2022-05-15 14:01:14 -0500

    Determinantes

--- Segundo Parcial/determinantes.c
+++ Segundo Parcial/determinantes.c
@@ -3,7 +3,7 @@
  *
  *	Descripción:
  *
- *	Modo de compilación: gcc -Wall determinantes.c -lm -o determinantes
+ *	Modo de compilación: gcc -Wall determinantes.c -lpthread -o determinantes
  *
  *	Modo de ejecución:	./determinates < n > <archivo.txt>
  *
@@ -14,26 +14,28 @@
  *
  *	Licencia: CC BY-NC-SA 4.0
  */
+
+#include <pthread.h>
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 
-#define MAX 100
-
 typedef struct {
-	int ope;
+	int resultado;
+	//int **cofactor;
+	int FilaMin;
 	int tamMatriz;
 	int **matriz;
 } datosMatriz;
 
-unsigned long long factorial(long long n);
+//unsigned long long factorial(long long n);
 int **creaMatriz(int tam);
 int determinante2X2(int **matriz);
 int **cofactor(int **matriz,int tam, int p, int q);
-int determinanteMC(int **matriz, int tam);
+//int determinanteMC(int **matriz, int tam);
+void *calculaDet(void *valor);
 
 int main(int argc, char const *argv[]){
-	int tam_matriz;
+	int tam_matriz, resultado=0;
 
 	if (argc == 1 || argc > 3){
 		printf("Debes colocar como argumento el tam de la matriz cuadrada < n > seguido del nombre del archivo <archivo.txt>\n");
@@ -41,8 +43,6 @@ int main(int argc, char const *argv[]){
 	}
 
 	tam_matriz = atoi(argv[1]);
-
-	printf("valor: %d\n", tam_matriz);
 
 	int **matriz, num;
 	matriz = creaMatriz(tam_matriz);
@@ -57,47 +57,64 @@ int main(int argc, char const *argv[]){
 		}
 	}
 
-	for (int i = 0; i < tam_matriz; i++){
-		for (int j = 0; j < tam_matriz; j++){
-			printf("%d\t", matriz[i][j]);
-		}
-		printf("\n");
-	}
-
 	fclose(fptr);
 
+	pthread_t tid[tam_matriz];
+	pthread_attr_t attr;
+	datosMatriz hilos[tam_matriz];
 
-	printf("tam matriz: %d\n", tam_matriz);
-	printf("Número de procesos: %lld\n", factorial(tam_matriz));
-	printf("Determinante: %d\n", determinanteMC(matriz,tam_matriz));
+	for(int i = 0; i < tam_matriz; i++){
+		pthread_attr_init(&attr);
 
+		// Establecemos los cofactores
+		hilos[i].matriz = cofactor(matriz,tam_matriz,0,i);
+		hilos[i].tamMatriz = tam_matriz-1;
+		if(i%2 != 0)
+			hilos[i].FilaMin = -matriz[0][i];
+		else
+			hilos[i].FilaMin = matriz[0][i];
 
+		//hilos[i].resultado *= hilos[i].FilaMin; 
+
+		pthread_create(&tid[i],&attr,calculaDet,&hilos[i]);
+	}
+
+	// espera terminación de hilos
+	for (int i = 0; i < tam_matriz; i++){
+		pthread_join(tid[i], NULL);
+	}
+
+	for (int i = 0; i < tam_matriz; i++){
+		resultado += hilos[i].resultado;
+	}
+
+	printf("El determinate de la matriz es = %d\n", resultado);
 	return 0;
 }
 
-/*Factorial de un número*/
-unsigned long long factorial(long long n){
-	if (n > MAX)
-		return -1;
+// /*Factorial de un número*/
+// unsigned long long factorial(long long n){
+// 	if (n > 1000)
+// 		return -1;
 
-	long long contador;
-	long double suma = 0;
+// 	long long contador;
+// 	long double suma = 0;
 
-	// Caso base
+// 	// Caso base
 
-	if (n == 0)
-		return 1;
+// 	if (n == 0)
+// 		return 1;
 
-	// Calcula la suma de los logaritmos
+// 	// Calcula la suma de los logaritmos
 
-	for (contador = 1; contador <= n; contador++){
-		suma = suma + log(contador);
-	}
+// 	for (contador = 1; contador <= n; contador++){
+// 		suma = suma + log(contador);
+// 	}
 
-	long double resultado = round(exp(suma)) / 2;
+// 	long double resultado = round(exp(suma)) / 2;
 
-	return (unsigned long long int)resultado;
-}
+// 	return (unsigned long long int)resultado;
+// }
 
 /*Crea Matriz*/
 int **creaMatriz(int tam){
@@ -124,36 +141,6 @@ int determinante2X2(int **matriz){
 	return resultado;
 }
 
-int determinanteMC(int **matriz, int tam){
-	int **auxMat;
-	int determinante=0;
-	int fila[tam];
-
-	auxMat = creaMatriz(tam-1);
-	if(tam == 2)
-		determinante = determinante2X2(matriz);
-	else{
-		// Calcula el signo
-		for(int i = 0; i<tam ; i++){
-			if(i%2 != 0){
-				fila[i] = -matriz[0][i];
-			}
-			else{
-				fila[i] = matriz[0][i];
-			}
-		}
-		
-		for(int j = 0; j<tam; j++){
-			// Obtenemos el cofactor de la matriz
-			auxMat = cofactor(matriz,tam,0,j);
-			// Calculo del determinante
-			determinante += fila[j] * determinanteMC(auxMat,tam-1);
-		}
-	}
-
-	return determinante;
-}
-
 int **cofactor(int **matriz,int tam, int p, int q){
 	int a,b;
 	int **matrizAux;
@@ -175,5 +162,79 @@ int **cofactor(int **matriz,int tam, int p, int q){
 	}
 
 	return matrizAux;
-
 }
+
+void *calculaDet(void *valor){
+	datosMatriz *datos;
+	datos = (datosMatriz *)valor;
+
+	pthread_t tid[datos->tamMatriz];
+	pthread_attr_t attr;
+	datosMatriz hilos[datos->tamMatriz];
+
+	if(datos->tamMatriz == 2){
+		datos->resultado = determinante2X2(datos->matriz);
+	}
+	else{
+
+		for(int i = 0; i < datos->tamMatriz; i++){
+			pthread_attr_init(&attr);
+
+			// Establecemos los cofactores
+			hilos[i].matriz = cofactor(datos->matriz,datos->tamMatriz,0,i);
+			hilos[i].tamMatriz = datos->tamMatriz-1;
+
+			if(i%2 != 0)
+				hilos[i].FilaMin = -datos->matriz[0][i];
+			else
+				hilos[i].FilaMin = datos->matriz[0][i];
+
+			
+			pthread_create(&tid[i],&attr,calculaDet,&hilos[i]);
+		}
+
+
+		// espera terminación de hilos
+		for (int i = 0; i < datos->tamMatriz; i++){
+			pthread_join(tid[i], NULL);
+		}
+		
+		
+	}
+
+	datos->resultado *= datos->FilaMin;
+
+	pthread_exit(0);
+}
+
+// int determinanteMC(int **matriz, int tam){
+// 	int **auxMat;
+// 	int determinante=0;
+// 	int fila[tam];
+
+
+// 	auxMat = creaMatriz(tam-1);
+// 	if(tam == 2)
+// 		determinante = determinante2X2(matriz);
+		
+// 	else{
+// 		// Calcula el signo
+// 		for(int i = 0; i<tam ; i++){
+// 			if(i%2 != 0){
+// 				fila[i] = -matriz[0][i];
+// 			}
+// 			else{
+// 				fila[i] = matriz[0][i];
+// 			}
+// 		}
+// 		// hilos agregar
+// 		for(int j = 0; j<tam; j++){
+// 			// Obtenemos el cofactor de la matriz
+// 			auxMat = cofactor(matriz,tam,0,j);
+// 			// Calculo del determinante
+// 			determinante += fila[j] * determinanteMC(auxMat,tam-1);
+// 		}
+// 	}
+
+// 	return determinante;
+// }
